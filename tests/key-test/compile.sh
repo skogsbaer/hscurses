6a74d7f5d70f7a67d574a1852fbdd5f1da6e3784
Stefan Wehr <mail@stefanwehr.de>
2011-09-18 12:23:44 +0200

    improvements to compile.sh script for key test

--- tests/key-test/compile.sh
+++ tests/key-test/compile.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-ghc --make -o key-test -package hscurses KeyTest.hs
+TOP=$(cd $(dirname $0)/../..; pwd -P)
+v=$(gawk '/^Version:/ { print $2 }' $TOP/hscurses.cabal)
+
+hscurses_pkg="hscurses-$v"
+echo "Using $hscurses_pkg"
+
+ghc --make -o key-test -package "$hscurses_pkg" -outputdir build KeyTest.hs
